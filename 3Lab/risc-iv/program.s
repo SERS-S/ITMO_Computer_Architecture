8d32d0434c91bb59752e94790ece53c1a34e9415
SER <104014082+SERS-S@users.noreply.github.com>
2026-05-09 16:02:23 +0300

    3Lab_risc-iv_fix

--- 3Lab/risc-iv/program.s
+++ 3Lab/risc-iv/program.s
@@ -1,6 +1,6 @@
 ; Upper_case_pstr
 
-; t1 - адрес начала буфера
+; t1 - адрес начала буфера / индекс буфера
 ; t2 - длина строки
 ; t3 - output адрес
 ; t4 - регистр для чтения символов
@@ -12,19 +12,25 @@ input_addr: .word 0x80
 output_word: .word 0x84
 
 .text
+    .org 0x88
 _start:
 
-    lui t0, %hi(input_addr)
-    addi t0, t0, %lo(input_addr)
-    lw t0, 0(t0)
+    addi t0, zero, 0x80
+    addi t3, zero, 0x84
 
-    lui sp, %hi(0x1000)
-    addi sp, sp, %lo(0x1000)
-
-    addi s1, zero, 0x20
+    addi s1, zero, 0x1f
 
     addi t1, zero, 0
     addi t2, zero, 0
+    addi t5, zero, '_'
+
+init_buffer:
+    bgt t1, s1, read_loop
+
+    sb t5, 0(t1)
+
+    addi t1, t1, 1
+    j init_buffer
 
 read_loop:
     lw t4, 0(t0)
@@ -33,17 +39,6 @@ read_loop:
     beq t4, t5, end
 
     beq t2, s1, overflow
-
-    addi sp, sp, -4
-    sw t4, 0(sp)
-    jal ra, uppercase_and_write
-    addi sp, sp, 4
-
-    addi t2, t2, 1
-    j read_loop
-
-uppercase_and_write:
-    lw t4, 0(sp)
 
     addi t5, zero, 'a'
     bgt t5, t4, write_char
@@ -54,24 +49,33 @@ uppercase_and_write:
     addi t4, t4, -32
 
 write_char:
-    lui t3, %hi(output_word)
-    addi t3, t3, %lo(output_word)
-    lw t3, 0(t3)
-    sb t4, 0(t3)
+    addi t1, t2, 1
+    sb t4, 0(t1)
 
-    jr ra
+    addi t2, t2, 1
+    j read_loop
 
 overflow:
-    lui t3, %hi(output_word)
-    addi t3, t3, %lo(output_word)
-    lw t3, 0(t3)
-
-    lui t5, 0xCCCCD
+    lui t5, 0xCCCCC
     addi t5, t5, 0xCCC
 
     sw t5, 0(t3)
 
-    j end
+    j halt
 
 end:
+    sb t2, 0(zero)
+
+    addi t1, zero, 1
+
+write_output:
+    bgt t1, t2, halt
+
+    lw t4, 0(t1)
+    sb t4, 0(t3)
+
+    addi t1, t1, 1
+    j write_output
+
+halt:
     halt
